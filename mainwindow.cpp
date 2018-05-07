a3e7515e4b73c0a0899f5b9552cfdfbe53a5fea4
sonichy <sonichy@163.com>
2018-05-07 12:55:18 +0800

    说明文档严重错误：帧内容 -> 帧内容大小; QString(TC->toUnicode(QByteArray)) -> TC->toUnicode(QByteArray)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -39,7 +39,7 @@ void MainWindow::open(QString filename)
     path = filename;
     setWindowTitle("MP3Tag - " + QFileInfo(filename).fileName());
     ui->textBrowser->setText("");
-    QTextCodec *TC = QTextCodec::codecForName("GBK");
+
     //Qt读写文件 https://blog.csdn.net/zhuyunfei/article/details/51249378
     //Mp3Tag读取 https://blog.csdn.net/junglesong/article/details/1747887
     //Mp3文件结构 https://blog.csdn.net/fulinwsuafcie/article/details/8972346
@@ -49,7 +49,8 @@ void MainWindow::open(QString filename)
     bool ok;
     qint64 pos,size;
     ID3 = QString(file.read(3));
-    if(ID3 == "ID3"){
+    if (ID3 == "ID3") {
+        QTextCodec *TC = QTextCodec::codecForName("GBK");
         ui->textBrowser->append(ID3);
         Ver = QString::number(file.read(1).toHex().toInt(&ok,16));
         ui->textBrowser->append("版本号：" + Ver);
@@ -63,31 +64,25 @@ void MainWindow::open(QString filename)
 //        QByteArray c = file.read(1);
 //        QByteArray d = file.read(1);
 //        qDebug() << a << b << c << d;
-        size = (file.read(1).toHex().toInt(&ok,16) & 0xEF) << 21 | (file.read(1).toHex().toInt(&ok,16) & 0xEF) << 14 | (file.read(1).toHex().toInt(&ok,16) & 0xEF) << 7 | file.read(1).toHex().toInt(&ok,16) & 0xEF;
-        //size = 20000;
-        qDebug() << "ID3Size" << size;
+        size = (file.read(1).toHex().toInt(&ok,16) & 0xEF) << 21 | (file.read(1).toHex().toInt(&ok,16) & 0xEF) << 14 | (file.read(1).toHex().toInt(&ok,16) & 0xEF) << 7 | file.read(1).toHex().toInt(&ok,16) & 0xEF;        
+        qDebug() << "ID3V2 Size:" << size;
         ui->textBrowser->append("大小：" + QString::number(size));
         while (file.pos() < size) {
             QString FTag(file.read(4));
             qint64 FSize = file.read(4).toHex().toLongLong(&ok,16);
             //qint64 FSize = file.read(1).toHex().toInt(&ok,16) << 24 | file.read(1).toHex().toInt(&ok,16) << 16 | file.read(1).toHex().toInt(&ok,16) << 8 | file.read(1).toHex().toInt(&ok,16);
-            qDebug() << FTag << FSize;
             Flag = QString::number(file.read(2).toHex().toInt(&ok,16));
             QByteArray BA = file.read(FSize);
             if (FTag == "APIC") {
                 BA = BA.right(FSize-14);
                 QPixmap pixmap;
                 ok = pixmap.loadFromData(BA);
-                qDebug() << "QPixmap.loadFromData(QByteArray)" << ok;
-                //pixmap.save("cover.jpg");
+                //qDebug() << "QPixmap.loadFromData(QByteArray)" << ok;
                 ui->label->setPixmap(pixmap);
-//                QFile file1("cover.jpg");
-//                file1.open(QIODevice::WriteOnly);
-//                file1.write(BA);
-//                file1.close();
                 break;
             } else {
-                ui->textBrowser->append(FTag + ": " + QString(TC->toUnicode(BA)));
+                qDebug() << FTag << FSize << BA;
+                ui->textBrowser->append(FTag + ": " +  BA);
             }
         }
     }else{
@@ -98,17 +93,18 @@ void MainWindow::open(QString filename)
     file.seek(pos);
     QString TAG,Title,Artist,Album,Year,Comment,Reserved,Track,Genre;
     TAG = QString(file.read(3));
-    if(TAG == "TAG"){
-        ui->textBrowser->append(TAG);
-        Title = QString(TC->toUnicode(file.read(30)));
+    if (TAG == "TAG") {
+        QTextCodec *TC = QTextCodec::codecForName("GBK");
+        ui->textBrowser->append("ID3V1");
+        Title = TC->toUnicode(file.read(30));
         ui->textBrowser->append("标题：" + Title);
-        Artist = QString(TC->toUnicode(file.read(30)));
+        Artist = TC->toUnicode(file.read(30));
         ui->textBrowser->append("歌手：" + Artist);
-        Album = QString(TC->toUnicode(file.read(30)));
+        Album = TC->toUnicode(file.read(30));
         ui->textBrowser->append("专辑：" + Album);
         Year = QString(file.read(4));
-        ui->textBrowser->append("年份："+Year);
-        Comment = QString(TC->toUnicode(file.read(28)));
+        ui->textBrowser->append("年份：" + Year);
+        Comment = TC->toUnicode(file.read(28));
         ui->textBrowser->append("备注：" + Comment);
         Reserved = QString(file.read(1));
         ui->textBrowser->append("保留：" + Reserved);
@@ -116,7 +112,7 @@ void MainWindow::open(QString filename)
         ui->textBrowser->append("音轨：" + Track);
         Genre = QString::number(file.read(1).toInt());
         ui->textBrowser->append("种类：" + Genre);
-    }else{
+    } else {
         ui->textBrowser->append("没有ID3V1");
     }
     file.close();
